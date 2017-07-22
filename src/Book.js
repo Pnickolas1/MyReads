a5f07d49a3b59dc1784169c92575b4f6512355a6
Peter Tountas <petertountas1@gmail.com>
2017-07-22 14:39:28 -0500

    added multipe files and components

---
+++ src/Book.js
@@ -0,0 +1,59 @@
+import React, { Component } from "react";
+
+class Book extends Component {
+    render() {
+        let book = this.props.book;
+        let imageLink = book.imageLinks
+            ? book.imageLinks.smallThumbnail
+            : "https://thumbs.dreamstime.com/t/what-book-was-open-hard-cover-blank-pages-question-mark-sign-written-right-page-isolated-white-67105612.jpg"
+        let title = book.title.length > 20 ? book.title.substring(0,20) + "..." : book.title;
+        let authors = "";
+
+        let shelf = this.props.selectValue
+            ? this.props.selectValue : book.shelf ? book.shelf : "none";
+        console.log(shelf)
+
+        if (book.authors) 
+            authors = 
+                book.authors.join(", ").length > 20 ? book.authors.join(", ").substring(0, 20) + "..."
+                : book.authors.join(", ");
+        
+        return (
+            <div>
+                <div className="book-top">
+                    <div
+                        className="book-cover"
+                        style={{
+                            width: 128,
+                            height: 193,
+                            backgroundImage: `url("${imageLink}")`
+                          }}
+                    />}
+                    <div className="book-shelf-changer">
+                        <select
+                            value={shelf}
+                            onChange={event =>
+                            this.props.onShelfChange(book, event.target.value)}
+                        >
+                        <option value="none" disabled>
+                                Move To...
+                            </option>
+                            <option value="currentlyReading">Currently Reading</option>
+                            <option value="wantToRead">Want To Read</option>
+                            <option value="read">Read</option>
+                        </select>
+                    </div>
+                </div>
+                <div className="book-title">
+                    {title}
+                </div>
+                <div className="book-authors">
+                    {authors}
+                </div>
+            </div> 
+                    
+        );
+  }
+}
+
+export default Book
---
+++ src/BookList.js
@@ -0,0 +1,91 @@
+import React, { Component } from 'react';
+import {Link} from 'react-router-dom';
+import * as BooksAPI from './BooksAPI';
+import BookShelf from './BookSelf';
+
+class BookList extends Component {
+    state = {
+        books: []
+    };
+
+    componentDidMount() {
+        BooksAPI.getAll().then(books => {
+            this.setState({
+                books: books
+            });
+        });
+    }
+
+    onShelfChange = (book, shelf ) => {
+        let books = this.state.books;
+        books.shelf = shelf;
+
+        //find the index of the book in the array (list in python)
+        let findBookIndex = books.findIndex(b => b.id == book.id);
+
+        books[findBookIndex] = book;
+
+        BooksAPI.update(book, shelf).then(result => {this.setState({
+            books: books
+        });
+    })
+    .catch(e => this.setState({ books: books }));
+ };
+
+
+render() {
+    let wantToReadBooks = this.books.filter(
+        book => book.shelf === 'wantToRead'
+    );
+
+    let currentlyReading = this.books.filter(
+        book => book.shelf === 'currentlyReading'        
+    );
+
+    let readBooks = this.books.filter(
+        book => book.shelf ==='read'
+    );
+
+    return (
+        <div className="list-books">
+            <div className="list-books-title">
+                <h1>MyReads</h1>
+        </div>
+        <div className="list-books-content">
+            <div>
+                <div className="bookshelf">
+                    <h2 className="bookshelf-title">Currently Reading</h2>
+                    <BookShelf
+                        books={currentlyReading}
+                        onShelfChange={this.onShelfChange}
+                        customClassName="bookshelf-books"
+                    />
+                </div>
+                <div className="bookshelf">
+                    <h2 className="bookshelf-title">Want To Read</h2>
+                    <BookShelf
+                        books={wantToReadBooks}
+                        onShelfChange={this.onShelfChange}
+                        customClassName="bookshelf-books"
+                    />
+                </div>
+                <div className="bookshelf">
+                    <h2 className="bookshelf-title">Read</h2>
+                    <BookShelf
+                        books={readBooks}
+                        onShelfChange={this.onShelfChange}
+                        customClassName="bookshelf-books"
+                    />
+                </div> 
+              </div>
+            <div>
+            <div className="open-search">
+                <Link to="/search">Add Book</Link>
+            </div>
+        </div>
+      );
+   }   
+}
+
+export default BookList;
+
